b3a2e7101723b211fc09fc8b3f6eb6464a5f62ff
Mike <mikechuks61@gmail.com>
2026-07-30 19:31:41 -0700

    done

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,7 +2,7 @@ export const contact = [
   {
     "id": 1,
     "title": "Get In Touch",
-    "description": "I'd love to hear from you! Whether you have a project in mind, a question about my services, or just want to connect, feel free to reach out. I'll get back to you as soon as possible.",
+    "description": "I'd love to hear from you! Whether you have a project in mind, a question about my services, or just want to connects, feel free to reach out. I'll get back to you as soon as possible.",
     "Address_title": "Address",
     "Address_img": "Location",
     "Address_desc": "123 Design Street, Creative District\nNew York, NY 10001\nUnited States",
